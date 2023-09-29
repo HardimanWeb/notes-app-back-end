c83555917fc936f4a619a3b613c3edb188d202b6
HardimanWeb <hardimansimamorah@gmail.com>
2023-09-29 23:36:29 +0700

    add code program

---
+++ src/notes.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+// eslint-disable-next-line linebreak-style
+// array untuk menampung objek catatan pada berkas notes.js.
+const notes = [];
+module.exports = notes;
